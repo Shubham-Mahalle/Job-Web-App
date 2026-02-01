57657a187c21104930d58246ed0115a7ba489ce8
Shubham-Mahalle <150221292+Shubham-Mahalle@users.noreply.github.com>
2026-02-01 18:11:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin:'http://localhost:5173',
+    origin:'https://career-connect-frontend-ajpa.onrender.com',
     credentials:true
 }
 
